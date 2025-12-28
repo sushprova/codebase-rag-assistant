254b51565fa7ce4e1fbdc917e72a726fa8e702d4
Sahil Patel <saheeelpatel@gmail.com>
2025-12-27 22:00:08 -0600

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
-import { Button } from "@/registry/new-york-v4/ui/button";
-import { toast } from "sonner";
+import { Button } from '@/registry/new-york-v4/ui/button';
+
+import { toast } from 'sonner';
 
 /**
  * The main page component that renders the HomePage component.
@@ -8,8 +9,7 @@ import { toast } from "sonner";
  * @returns {JSX.Element} The rendered HomePage component.
  */
 const Page = () => {
-	return <Button onClick={() => toast.success('This is a test toast from the Page component!')		
-	} >Test Btn</Button>;
+	return <Button onClick={() => toast.success('This is a test toast from the Page component!')}>Test Btn</Button>;
 };
 
 export default Page;
