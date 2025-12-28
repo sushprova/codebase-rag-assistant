0c344b3ea88b0ebf5d47758a1ca91bdbd7269b11
Sahil Patel <saheeelpatel@gmail.com>
2025-12-27 22:00:01 -0600

    update repo to add shadcn and other start files

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
+import initializeBundleAnalyzer from '@next/bundle-analyzer';
+
+// https://www.npmjs.com/package/@next/bundle-analyzer
+const withBundleAnalyzer = initializeBundleAnalyzer({
+    enabled: process.env.BUNDLE_ANALYZER_ENABLED === 'true'
+});
+
+// https://nextjs.org/docs/pages/api-reference/next-config-js
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: 'standalone'
 };
 
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig);
---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+import { Button } from "@/registry/new-york-v4/ui/button";
+import { toast } from "sonner";
+
+/**
+ * The main page component that renders the HomePage component.
+ *
+ * @returns {JSX.Element} The rendered HomePage component.
+ */
+const Page = () => {
+	return <Button onClick={() => toast.success('This is a test toast from the Page component!')		
+	} >Test Btn</Button>;
+};
+
+export default Page;
